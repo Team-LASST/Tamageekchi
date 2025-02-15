e3f10fb2f9fc3f9034f2829e8b04893ef5a63a9b
butter9fe <82465661+butter9fe@users.noreply.github.com>
2025-02-15 19:53:11 +0800

    Feat: Sign In Page Design
    F

--- Assets/Scripts/PlayFab/SignUpManager.cs
+++ Assets/Scripts/PlayFab/SignUpManager.cs
@@ -10,29 +10,28 @@ public class SignUpManager : MonoBehaviour
     [SerializeField] PopupManager popupManager;
     [SerializeField] SplashManager splashManager;
     [Header("Fields")]
-    [SerializeField] TMP_InputField field_fullname;
-    [SerializeField] TMP_InputField field_email, field_pw, field_confirmpw, field_phone;
+    [SerializeField] TMP_InputField field_firstname;
+    [SerializeField] TMP_InputField field_lastname, field_email, field_pw, field_confirmpw;
     [Header("Errors")]
     [SerializeField] TMP_Text error_name;
-    [SerializeField] TMP_Text error_email, error_pw, error_confirmpw, error_phone;
+    [SerializeField] TMP_Text error_email, error_pw, error_confirmpw;
     [Header("Buttons")]
     [SerializeField] ButtonLoader button_register;
     [SerializeField] Button button_showpw, button_showconfirmpw;
 
     private void OnDisable()
     {
-        if (field_fullname == null)
+        if (field_firstname == null)
             return;
 
         // Reset fields on disable
-        field_fullname.text = field_email.text = field_pw.text = field_confirmpw.text = "";
+        field_firstname.text = field_lastname.text = field_email.text = field_pw.text = field_confirmpw.text = "";
 
         // Hide errors
         error_name.gameObject.SetActive(false);
         error_email.gameObject.SetActive(false);
         error_pw.gameObject.SetActive(false);
         error_confirmpw.gameObject.SetActive(false);
-        error_phone.gameObject.SetActive(false);
     }
 
     private bool ValidateFields()
@@ -41,7 +40,12 @@ public class SignUpManager : MonoBehaviour
         error_email.gameObject.SetActive(false);
         error_pw.gameObject.SetActive(false);
         error_confirmpw.gameObject.SetActive(false);
-        error_phone.gameObject.SetActive(false);
+
+        if (field_firstname.text.Trim().Length == 0 || field_lastname.text.Trim().Length == 0)
+        {
+            error_name.gameObject.SetActive(true);
+            return false;
+        }
 
         if (!Regex.IsMatch(field_email.text, splashManager.EmailRegex))
         {
@@ -49,22 +53,8 @@ public class SignUpManager : MonoBehaviour
             return false;
         }
 
-        if (field_pw.text.Length < 8)
+        if (field_pw.text.Length < 6)
         {
-            error_pw.text = "Ensure your password has at least 8 characters.";
-            error_pw.gameObject.SetActive(true);
-            return false;
-        }
-
-        int charaGroupsMatched =
-            System.Convert.ToInt32(Regex.IsMatch(field_pw.text, "[a-z]")) +
-            System.Convert.ToInt32(Regex.IsMatch(field_pw.text, "[A-Z]")) +
-            System.Convert.ToInt32(Regex.IsMatch(field_pw.text, "[0-9]")) +
-            System.Convert.ToInt32(Regex.IsMatch(field_pw.text, "[[!@#$%&*()_+=|<>?{}\\[\\]~-]"));
-
-        if (charaGroupsMatched < 3)
-        {
-            error_pw.text = "Ensure your password contains at least 3 of the following 4 types of characters: lower case letters [a-z], upper case letters [A-Z], numbers [0-9], special characters [eg: !@#$%^&*].";
             error_pw.gameObject.SetActive(true);
             return false;
         }
@@ -72,18 +62,6 @@ public class SignUpManager : MonoBehaviour
         if (field_pw.text != field_confirmpw.text)
         {
             error_confirmpw.gameObject.SetActive(true);
-            return false;
-        }
-
-        if (field_fullname.text.Trim().Length == 0)
-        {
-            error_name.gameObject.SetActive(true);
-            return false;
-        }
-
-        if (!Regex.IsMatch(field_phone.text, @"[8-9]\d{7}"))
-        {
-            error_phone.gameObject.SetActive(true);
             return false;
         }
 
@@ -108,7 +86,7 @@ public class SignUpManager : MonoBehaviour
         {
             Email = field_email.text,
             Password = field_pw.text,
-            DisplayName = field_fullname.text,
+            DisplayName = $"{field_firstname.text} {field_lastname.text}",
             RequireBothUsernameAndEmail = false
         };
 
@@ -127,14 +105,14 @@ public class SignUpManager : MonoBehaviour
     void OnRegSucc(RegisterPlayFabUserResult r)
     {
         popupManager.ShowMessage("Success!", "Please login with your new account.", "OK", false, () => splashManager.OnSwitchPanel("Login"), () => splashManager.OnSwitchPanel("Login"));
-        CloudScriptManager.Instance.ExecBasicCoudScriptFunction(CloudScriptType.OnUserRegister, null, e => OnError("Error assigning box ID.", e));
+        CloudScriptManager.Instance.ExecBasicCoudScriptFunction(CloudScriptType.OnUserRegister, null, e => OnError("Error assigning default Geeklings", e));
     }
 
     void OnError(string errorTitle, PlayFabError e)
     {
         if (e.Error.ToString() == "UsernameNotAvailable")
         {
-            popupManager.ShowMessage("Oops!", "You already have an account under this phone number.", "OK", false);
+            popupManager.ShowMessage("Oops!", "You already have an account under this email.", "OK", false);
         }
         else
         {
